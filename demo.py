c009ed9900357d41647614f46d5e2bb3e8428262
Sahit Chintalapudi <chsahit@gmail.com>
2024-07-30 20:12:31 -0400

    add code for standalone training

---
+++ demo.py
@@ -0,0 +1,313 @@
+# diffusion policy import
+from typing import Tuple, Sequence, Dict, Union, Optional
+import numpy as np
+import torch
+import torch.nn as nn
+import math
+import collections
+import zarr
+from diffusers.schedulers.scheduling_ddpm import DDPMScheduler
+from diffusers.training_utils import EMAModel
+from diffusers.optimization import get_scheduler
+from tqdm.auto import tqdm
+
+# env import
+import gym
+from gym import spaces
+import pygame
+import pymunk
+import pymunk.pygame_util
+from pymunk.space_debug_draw_options import SpaceDebugColor
+from pymunk.vec2d import Vec2d
+import shapely.geometry as sg
+import cv2
+import skimage.transform as st
+import gdown
+import os
+from skvideo.io import vwrite
+
+from push_t_env import PushTEnv
+from pih_env import PIHEnv
+from conditional_residual import ConditionalUnet1D
+from push_t_statedataset import *
+
+# download demonstration data from Google Drive
+dataset_path = "pusht_cchi_v7_replay.zarr.zip"
+if not os.path.isfile(dataset_path):
+    id = "1KY1InLurpMvJDRb14L9NlXT_fEsCvVUq&confirm=t"
+    gdown.download(id=id, output=dataset_path, quiet=False)
+
+# parameters
+pred_horizon = 16
+obs_horizon = 2
+action_horizon = 8
+#|o|o|                             observations: 2
+#| |a|a|a|a|a|a|a|a|               actions executed: 8
+#|p|p|p|p|p|p|p|p|p|p|p|p|p|p|p|p| actions predicted: 16
+
+# create dataset from file
+dataset = PushTStateDataset(
+    dataset_path="buffer",
+    pred_horizon=pred_horizon,
+    obs_horizon=obs_horizon,
+    action_horizon=action_horizon
+)
+# save training data statistics (min, max) for each dim
+stats = dataset.stats
+
+# create dataloader
+dataloader = torch.utils.data.DataLoader(
+    dataset,
+    batch_size=256,
+    num_workers=1,
+    shuffle=True,
+    # accelerate cpu-gpu transfer
+    pin_memory=True,
+    # don't kill worker process afte each epoch
+    persistent_workers=True
+)
+print(f"{type(dataset.normalized_train_data['action'])=}")
+print(f"{len(dataset)=}")
+# visualize data in batch
+batch = next(iter(dataloader))
+print("batch['obs'].shape:", batch['obs'].shape)
+print("batch['action'].shape", batch['action'].shape)
+
+obs_dim = 8
+action_dim = 2
+
+# create network object
+noise_pred_net = ConditionalUnet1D(
+    input_dim=action_dim,
+    global_cond_dim=obs_dim*obs_horizon
+)
+
+# example inputs
+noised_action = torch.randn((1, pred_horizon, action_dim))
+obs = torch.zeros((1, obs_horizon, obs_dim))
+diffusion_iter = torch.zeros((1,))
+
+# the noise prediction network
+# takes noisy action, diffusion iteration and observation as input
+# predicts the noise added to action
+noise = noise_pred_net(
+    sample=noised_action,
+    timestep=diffusion_iter,
+    global_cond=obs.flatten(start_dim=1))
+
+# illustration of removing noise
+# the actual noise removal is performed by NoiseScheduler
+# and is dependent on the diffusion noise schedule
+denoised_action = noised_action - noise
+
+# for this demo, we use DDPMScheduler with 100 diffusion iterations
+num_diffusion_iters = 100
+noise_scheduler = DDPMScheduler(
+    num_train_timesteps=num_diffusion_iters,
+    # the choise of beta schedule has big impact on performance
+    # we found squared cosine works the best
+    beta_schedule='squaredcos_cap_v2',
+    # clip output to [-1,1] to improve stability
+    clip_sample=True,
+    # our network predicts noise (instead of denoised action)
+    prediction_type='epsilon'
+)
+
+# device transfer
+device = torch.device('cuda')
+_ = noise_pred_net.to(device)
+
+
+num_epochs = 100
+
+# Exponential Moving Average
+# accelerates training and improves stability
+# holds a copy of the model weights
+ema = EMAModel(
+    parameters=noise_pred_net.parameters(),
+    power=0.75)
+
+# Standard ADAM optimizer
+# Note that EMA parametesr are not optimized
+optimizer = torch.optim.AdamW(
+    params=noise_pred_net.parameters(),
+    lr=1e-4, weight_decay=1e-6)
+
+# Cosine LR schedule with linear warmup
+lr_scheduler = get_scheduler(
+    name='cosine',
+    optimizer=optimizer,
+    num_warmup_steps=500,
+    num_training_steps=len(dataloader) * num_epochs
+)
+
+with tqdm(range(num_epochs), desc='Epoch') as tglobal:
+    # epoch loop
+    for epoch_idx in tglobal:
+        epoch_loss = list()
+        # batch loop
+        with tqdm(dataloader, desc='Batch', leave=False) as tepoch:
+            for nbatch in tepoch:
+                # data normalized in dataset
+                # device transfer
+                nobs = nbatch['obs'].to(device)
+                naction = nbatch['action'].to(device)
+                B = nobs.shape[0]
+
+                # observation as FiLM conditioning
+                # (B, obs_horizon, obs_dim)
+                obs_cond = nobs[:,:obs_horizon,:]
+                # (B, obs_horizon * obs_dim)
+                obs_cond = obs_cond.flatten(start_dim=1)
+
+                # sample noise to add to actions
+                noise = torch.randn(naction.shape, device=device)
+
+                # sample a diffusion iteration for each data point
+                timesteps = torch.randint(
+                    0, noise_scheduler.config.num_train_timesteps,
+                    (B,), device=device
+                ).long()
+
+                # add noise to the clean images according to the noise magnitude at each diffusion iteration
+                # (this is the forward diffusion process)
+                noisy_actions = noise_scheduler.add_noise(
+                    naction, noise, timesteps)
+
+                # predict the noise residual
+                noise_pred = noise_pred_net(
+                    noisy_actions, timesteps, global_cond=obs_cond)
+
+                # L2 loss
+                loss = nn.functional.mse_loss(noise_pred, noise)
+
+                # optimize
+                loss.backward()
+                optimizer.step()
+                optimizer.zero_grad()
+                # step lr scheduler every batch
+                # this is different from standard pytorch behavior
+                lr_scheduler.step()
+
+                # update Exponential Moving Average of the model weights
+                ema.step(noise_pred_net.parameters())
+
+                # logging
+                loss_cpu = loss.item()
+                epoch_loss.append(loss_cpu)
+                tepoch.set_postfix(loss=loss_cpu)
+        tglobal.set_postfix(loss=np.mean(epoch_loss))
+
+# Weights of the EMA model
+# is used for inference
+ema_noise_pred_net = noise_pred_net
+ema.copy_to(ema_noise_pred_net.parameters())
+
+load_pretrained = False
+if load_pretrained:
+  ckpt_path = "pusht_state_100ep.ckpt"
+  if not os.path.isfile(ckpt_path):
+      id = "1mHDr_DEZSdiGo9yecL50BBQYzR8Fjhl_&confirm=t"
+      gdown.download(id=id, output=ckpt_path, quiet=False)
+
+  state_dict = torch.load(ckpt_path, map_location='cuda')
+  ema_noise_pred_net = noise_pred_net
+  ema_noise_pred_net.load_state_dict(state_dict)
+  print('Pretrained weights loaded.')
+else:
+  print("Skipped pretrained weight loading.")
+
+
+
+# limit enviornment interaction to 200 steps before termination
+max_steps = 400
+env = PIHEnv()
+# use a seed >200 to avoid initial states seen in the training dataset
+env.seed(100001)
+
+# get first observation
+obs, info = env.reset()
+
+# keep a queue of last 2 steps of observations
+obs_deque = collections.deque(
+    [obs] * obs_horizon, maxlen=obs_horizon)
+# save visualization and rewards
+imgs = [env.render(mode='rgb_array')]
+rewards = list()
+done = False
+step_idx = 0
+
+with tqdm(total=max_steps, desc="Eval PushTStateEnv") as pbar:
+    while not done:
+        B = 1
+        # stack the last obs_horizon (2) number of observations
+        obs_seq = np.stack(obs_deque)
+        # normalize observation
+        nobs = normalize_data(obs_seq, stats=stats['obs'])
+        # device transfer
+        nobs = torch.from_numpy(nobs).to(device, dtype=torch.float32)
+
+        # infer action
+        with torch.no_grad():
+            # reshape observation to (B,obs_horizon*obs_dim)
+            obs_cond = nobs.unsqueeze(0).flatten(start_dim=1)
+
+            # initialize action from Guassian noise
+            noisy_action = torch.randn(
+                (B, pred_horizon, action_dim), device=device)
+            naction = noisy_action
+
+            # init scheduler
+            noise_scheduler.set_timesteps(num_diffusion_iters)
+
+            for k in noise_scheduler.timesteps:
+                # predict noise
+                noise_pred = ema_noise_pred_net(
+                    sample=naction,
+                    timestep=k,
+                    global_cond=obs_cond
+                )
+
+                # inverse diffusion step (remove noise)
+                naction = noise_scheduler.step(
+                    model_output=noise_pred,
+                    timestep=k,
+                    sample=naction
+                ).prev_sample
+
+        # unnormalize action
+        naction = naction.detach().to('cpu').numpy()
+        # (B, pred_horizon, action_dim)
+        naction = naction[0]
+        action_pred = unnormalize_data(naction, stats=stats['action'])
+
+        # only take action_horizon number of actions
+        start = obs_horizon - 1
+        end = start + action_horizon
+        action = action_pred[start:end,:]
+        # (action_horizon, action_dim)
+
+        # execute action_horizon number of steps
+        # without replanning
+        for i in range(len(action)):
+            # stepping env
+            obs, reward, done, _, info = env.step(action[i])
+            # save observations
+            obs_deque.append(obs)
+            # and reward/vis
+            rewards.append(reward)
+            imgs.append(env.render(mode='rgb_array'))
+
+            # update progress bar
+            step_idx += 1
+            pbar.update(1)
+            pbar.set_postfix(reward=reward)
+            if step_idx > max_steps:
+                done = True
+            if done:
+                break
+
+# print out the maximum target coverage
+print('Score: ', max(rewards))
+vwrite("vis.mp4", imgs)
+
